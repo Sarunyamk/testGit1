f9c275f5d9ddd73bec4ebcec5554d1ea00802f08
sakkarin32 <sakkarin_32@hotmail.com>
2025-05-10 15:09:34 +0700

    Add many HR

--- src/assets/something.jsx
+++ src/assets/something.jsx
@@ -4,16 +4,17 @@ const something = () => {
   return (
     <div>
       <h1>something right that!</h1>
+      <hr />
       <span>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a
         dolorum animi porro blanditiis incidunt aspernatur iure quidem, impedit
-        illo saepe rerum deleniti quo doloribus delectus, eaque iste soluta ad!
       </span>
+      <hr />
       <span>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a
         dolorum animi porro blanditiis incidunt aspernatur iure quidem, impedit
         illo saepe rerum deleniti quo doloribus delectus, eaque iste soluta ad!
       </span>
+      <hr />
       <span>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a
         dolorum animi porro blanditiis incidunt aspernatur iure quidem, impedit
