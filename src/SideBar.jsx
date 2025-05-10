cfe6e8694630a5e77c0e39ead3b4b1425ddea676
Sarunyamk <Sarunya46mk@gmail.com>
2025-05-10 14:35:21 +0700

    fix : test number

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -8,7 +8,9 @@ const SideBar = () => {
       <p>This is the sidebar content.</p>
       <p>This is the sidebar content.</p>
       <p>This is the sidebar content.</p>
-      <h2>อะไรกันเนียย</h2>
+      <h2>อะไรกันเนียย  จะเอาอันไหนดี ลองเทสดู มีหลายอัน</h2>
+      <h2>1234546</h2>
+      <h3>1234546</h3>
     </div>
   );
 };
