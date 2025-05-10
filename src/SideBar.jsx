d1a5deacb19e83499f15fc6d8aaa76354032acf3
sakkarin32 <sakkarin_32@hotmail.com>
2025-05-10 14:39:38 +0700

    add h4,h5,h6

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -8,9 +8,12 @@ const SideBar = () => {
       <p>This is the sidebar content.</p>
       <p>This is the sidebar content.</p>
       <p>This is the sidebar content.</p>
-      <h2>อะไรกันเนียย  จะเอาอันไหนดี ลองเทสดู มีหลายอัน</h2>
+      <h2>อะไรกันเนียย จะเอาอันไหนดี ลองเทสดู มีหลายอัน</h2>
       <h2>1234546</h2>
       <h3>1234546</h3>
+      <h4>44444</h4>
+      <h5>55555</h5>
+      <h6>66666</h6>
     </div>
   );
 };
