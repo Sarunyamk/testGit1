0e827ee0ab742f85601b6fdc73711983259fd4b5
chawsakkarin <chawsakkarin@gmail.com>
2025-05-10 14:12:57 +0700

    Update Sidebar.

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const SideBar = () => {
-    return (
-        <div>
-            <h1>SideBar</h1>
-            <p>This is the sidebar content.</p>
-        </div>
+  return (
+    <div>
+      <h1>SideBar</h1>
+      <p>This is the sidebar content.</p>
+      <p>This is the sidebar content.</p>
+      <p>This is the sidebar content.</p>
+      <p>This is the sidebar content.</p>
+    </div>
+  );
+};
 
-    )
-}
-
-export default SideBar  
+export default SideBar;
