ce9fcb1fc2a56e9cf0a3c37208bd0c93482749ce
sakkarin32 <sakkarin_32@hotmail.com>
2025-05-10 15:08:31 +0700

    Add span

--- src/assets/something.jsx
+++ src/assets/something.jsx
@@ -4,6 +4,21 @@ const something = () => {
   return (
     <div>
       <h1>something right that!</h1>
+      <span>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a
+        dolorum animi porro blanditiis incidunt aspernatur iure quidem, impedit
+        illo saepe rerum deleniti quo doloribus delectus, eaque iste soluta ad!
+      </span>
+      <span>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a
+        dolorum animi porro blanditiis incidunt aspernatur iure quidem, impedit
+        illo saepe rerum deleniti quo doloribus delectus, eaque iste soluta ad!
+      </span>
+      <span>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a
+        dolorum animi porro blanditiis incidunt aspernatur iure quidem, impedit
+        illo saepe rerum deleniti quo doloribus delectus, eaque iste soluta ad!
+      </span>
     </div>
   );
 };
