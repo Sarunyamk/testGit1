6ed54507e3cf983a692a5a5444ffb62587fb37e9
Sarunyamk <Sarunya46mk@gmail.com>
2025-05-10 13:53:38 +0700

    fix : เปลี่ยนแปลงไฟล์ main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Header from './header.jsx'
+import SideBar from './SideBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Header />
+    <SideBar />
   </StrictMode>,
 )
