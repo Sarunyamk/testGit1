f98095cfd0e5f753b0558a0c61880683b2707593
Sarunyamk <Sarunya46mk@gmail.com>
2025-05-10 13:59:06 +0700

    feat : add sidebar

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const SideBar = () => {
     return (
-        <div>SideBar</div>
+        <div>
+            <h1>SideBar</h1>
+            <p>This is the sidebar content.</p>
+        </div>
+
     )
 }
 
